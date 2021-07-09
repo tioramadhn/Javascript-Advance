59fd7f6f800b2beb782f8dba93a7f415d8f3a7f1
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-07-09 14:55:37 +0700

    3.2 Arrow Function (Advance)

--- arrow.js
+++ arrow.js
@@ -13,7 +13,7 @@
 
 // console.log(tampilNama());
 
-let mahasiswa = ["Tio Ramadhan", "luthfi arifandi", "Yaser"];
+// let mahasiswa = ["Tio Ramadhan", "luthfi arifandi", "Yaser"];
 
 // let lengthMhs = mahasiswa.map(function(nama){
 //     return nama.length;
@@ -21,9 +21,24 @@ let mahasiswa = ["Tio Ramadhan", "luthfi arifandi", "Yaser"];
 
 // let  lengthMhs = mahasiswa.map(nama => nama.length)
 
-let  lengthMhs = mahasiswa.map(nama => ({
-    nama, 
-    panjangKarakter: nama.length
-}))
+// let  lengthMhs = mahasiswa.map(nama => ({
+//     nama, 
+//     panjangKarakter: nama.length
+// }))
 
-console.table(lengthMhs);
+// console.table(lengthMhs);
+
+
+const box = document.querySelector('.box');
+
+box.addEventListener('click', function() {
+    let satu = 'size';
+    let dua = 'caption';
+    if(this.classList.contains(satu)){
+        [satu,dua] = [dua, satu];
+    }
+    this.classList.toggle(satu);
+    setTimeout(()=>{
+        this.classList.toggle(dua)
+    },600);
+})
