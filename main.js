8be6cf6383ed88932a91af255b05573db76331c2
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-07-09 08:36:54 +0700

    1.2 Object.Create()

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Cara membuat ojek pada js
 // 1. Objek literal
+// PROBLEM : tidak efektif untuk objek yang banyak
 // let mahasiswa = {
 //     nama: 'Tio Ramadhan',
 //     energi: 10,
@@ -11,38 +12,48 @@
 
 
 // 2. Function declaration->>Harus di instansiasikan dan return
-// function mahasiswa(nama, energi){
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
-//     mahasiswa.makan = function(porsi){
-//         this.energi += porsi;
-//         console.log(`Halo ${this.nama}, Selamat Makan`);
-//     }
-//     mahasiswa.main = function(waktu){
-//         this.energi -= waktu;
-//         console.log(`Halo ${this.nama}, Selamat bermain`)
-//     }
 
-//     return mahasiswa;
-// }
-
-// let Tio = mahasiswa('Tio Ramadhan', 500);
-
-// 3. Constructor function ->> tanpa inisiasi objek-keyword new
-function mahasiswa(nama, energi){
-    this.nama = nama;
-    this.energi = energi;
-    this.makan = function(porsi){
+const methodMahasiswa = {
+    makan: function(porsi){
         this.energi += porsi;
         console.log(`Halo ${this.nama}, Selamat Makan`);
-    }
-    this.main = function(waktu){
+    },
+    main: function(waktu){
         this.energi -= waktu;
-        console.log(`Halo ${this.nama}, Selamat bermain`)
+        console.log(`Halo ${this.nama}, Selamat bermain`);
+    },
+    tidur: function(jam){
+        this.energi += jam;
+        console.log(`Halo ${this.nama}, Selamat tidur`);
     }
 }
 
-let Tio = new mahasiswa('Tio Ramadhan', 500);
+function mahasiswa(nama, energi){
+    // OBJECT.CREATE
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    
+
+    return mahasiswa;
+}
+
+let Tio = mahasiswa('Tio Ramadhan', 500);
+
+// 3. Constructor function ->> tanpa inisiasi objek-keyword new
+// function mahasiswa(nama, energi){
+//     this.nama = nama;
+//     this.energi = energi;
+//     this.makan = function(porsi){
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, Selamat Makan`);
+//     }
+//     this.main = function(waktu){
+//         this.energi -= waktu;
+//         console.log(`Halo ${this.nama}, Selamat bermain`)
+//     }
+// }
+
+// let Tio = new mahasiswa('Tio Ramadhan', 500);
 
 
