aeab1343e7347104ccd91b93114aefeb456f72a2
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-07-09 10:27:09 +0700

    2.2 Closure

--- execution.js
+++ execution.js
@@ -8,17 +8,51 @@
 // semua vraiabel akan disi undefined dan function akan di isi fn bersangkutan
 
 
-function satu(){
-    var nama = "tio";
-    console.log(nama);
+// function satu(){
+//     var nama = "tio";
+//     console.log(nama);
+// }
+
+// function dua(){
+//     console.log(nama);
+// }
+
+// console.log(nama);
+// var nama = "lutfi";
+// satu();
+// dua("Yaser");
+// console.log(nama);
+
+
+// CLOSURE
+// Factoring or Priavate Method
+function sayHello(waktu){
+    return function(nama){
+        return `Halo ${nama}, Selamat ${waktu}`;
+    }
 }
 
-function dua(){
-    console.log(nama);
-}
+let selamatPagi = sayHello('Pagi');
+let selamatSiang = sayHello('Siang');
+let selamatMalam = sayHello('Malam');
 
-console.log(nama);
-var nama = "lutfi";
-satu();
-dua("Yaser");
-console.log(nama);
+// console.dir(selamatPagi());
+// console.log(selamatMalam('Tio'));
+
+
+
+// Invoke
+let a = (function add(){
+    let counter = 0;
+    return function(){
+        return ++counter;
+    }
+})();
+
+
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
